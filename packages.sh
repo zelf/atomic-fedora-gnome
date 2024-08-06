e4bca9fd1dd757790b6d11fb01c488afafaeac59
zelf <zelf@startmail.com>
2024-08-07 01:26:49 +0200

    fix package list

--- packages.sh
+++ packages.sh
@@ -4,28 +4,32 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-# build list of all packages requested for inclusion
-INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
+# Helper function to parse included/excluded packages
+parse_packages() {
+    local key="$1"
+    jq -r "[
+        (.all.${key} | (.all, select(.\"${IMAGE_NAME}\" != null).\"${IMAGE_NAME}\")[]), 
+        (select(.\"${FEDORA_MAJOR_VERSION}\" != null).\"${FEDORA_MAJOR_VERSION}\".${key} | (.all, select(.\"${IMAGE_NAME}\" != null).\"${IMAGE_NAME}\")[])] 
+        | sort | unique[]" /ctx/packages.json
+}
 
-# build list of all packages requested for exclusion
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
+# Build list of all packages requested for inclusion
+INCLUDED_PACKAGES=($(parse_packages "include"))
 
+# Build list of all packages requested for exclusion
+EXCLUDED_PACKAGES=($(parse_packages "exclude"))
 
-# ensure exclusion list only contains packages already present on image
+# Ensure exclusion list only contains packages already present on image
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
 fi
 
-# simple case to install where no packages need excluding
+# Simple case to install where no packages need excluding
 if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -eq 0 ]]; then
     rpm-ostree install \
         ${INCLUDED_PACKAGES[@]}
 
-# install/excluded packages both at same time
+# Install/excluded packages both at same time
 elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     rpm-ostree override remove \
         ${EXCLUDED_PACKAGES[@]} \
@@ -33,20 +37,17 @@ elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]];
 
 else
     echo "No packages to install."
-
 fi
 
-# check if any excluded packages are still present
+# Check if any excluded packages are still present
 # (this can happen if an included package pulls in a dependency)
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
+EXCLUDED_PACKAGES=($(parse_packages "exclude"))
 
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
 fi
 
-# remove any excluded packages which are still present on image
+# Remove any excluded packages which are still present on image
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     rpm-ostree override remove \
         ${EXCLUDED_PACKAGES[@]}
