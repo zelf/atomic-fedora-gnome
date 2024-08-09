e7aea2ea018999e41a8b1cc48fea60d4dec6f2bd
zelf <zelf@users.noreply.github.com>
2024-08-10 00:29:26 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 set -ouex pipefail
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/docker-ce.repo
+
+for i in /etc/yum.repos.d/rpmfusion-*; do
+    sed -i 's@enabled=1@enabled=0@g' "$i"
+done:w
