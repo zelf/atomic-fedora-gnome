4512d32f462ba2e5b481e88e6bd6c532b4949dff
zelf <zelf@users.noreply.github.com>
2024-08-12 23:34:54 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,3 +9,7 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/docker-ce.repo
 for i in /etc/yum.repos.d/rpmfusion-*; do
     sed -i 's@enabled=1@enabled=0@g' "$i"
 done
+
+systemctl enable docker.socket
+systemctl enable podman.socket
+systemctl enable zelf-groups.service
